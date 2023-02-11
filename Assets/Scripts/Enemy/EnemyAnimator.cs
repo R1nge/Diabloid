90525b407300bc54eb42af5746f745b7456bc0f4
R1nge <r1ngegamedev@gmail.com>
2023-02-12 01:40:43 +0300

    feat: enemy animator

---
+++ Assets/Scripts/Enemy/EnemyAnimator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Enemy
+{
+    public class EnemyAnimator : MonoBehaviour
+    {
+        //TODO: state machine
+        [SerializeField] private Animator animator;
+        [SerializeField] private NavMeshAgent agent;
+        private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+        
+        private void Update() => animator.SetBool(IsWalking, agent.velocity.magnitude > 0);
+    }
+}
