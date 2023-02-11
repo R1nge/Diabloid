8b40522925f01e913b746f8882fb05e1a70cdf53
R1nge <r1ngegamedev@gmail.com>
2023-02-12 00:49:59 +0300

    Imported models, animations (Idle, Walking)

---
+++ Assets/Scripts/Character/CharacterAnimator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Character
+{
+    public class CharacterAnimator : MonoBehaviour
+    {
+        [SerializeField] private Animator animator;
+        [SerializeField] private NavMeshAgent agent;
+        private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+
+        private void Update() => animator.SetBool(IsWalking, agent.velocity.magnitude > 0);
+    }
+}
